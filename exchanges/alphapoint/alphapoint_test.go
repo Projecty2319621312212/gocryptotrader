baae0e5cc62c03eedf71b455ee8d2e2489b7ae80
Ryan O'Hara-Reid <oharareid.ryan@gmail.com>
2017-04-06 13:34:47 +1000

    Added test for SetDefaults

--- exchanges/alphapoint/alphapoint_test.go
+++ exchanges/alphapoint/alphapoint_test.go
@@ -1,7 +1,11 @@
 package alphapoint
 
-import "testing"
+import (
+	// "github.com/thrasher-/gocryptotrader/exchanges/alphapoint" //If we were outside of the folder we shoud declare the package here.
+	"testing"
+)
 
-func TestOooooWhatAreWeTestingSoSpooky(t *testing.T) {
-	t.Log("SO SPOOKY NOOO AHHH")
+func TestSetDefaults(t *testing.T) {
+	SetDefaults := Alphapoint{} // So I set an empty Alphapoint struct with member functions to setdefaults.
+	SetDefaults.SetDefaults()   //so I acesss setdefaults struct into its member function and setdefaults should run.
 }
